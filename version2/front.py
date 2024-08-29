ea25a0bac45c9edd77477ea4a920a8071ec9058b
duartebianca <biancaduarte1914@gmail.com>
2024-08-29 16:29:20 -0300

    add styling functions, change layout, add about section

--- version2/front.py
+++ version2/front.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import streamlit as st
 import time
-import matplotlib.pyplot as plt
-from utils import Utils
-import numpy as np
 import plotly.graph_objs as go
-import plotly.express as px
 import altair as alt
+from utils import Utils
 
+# Graphical Functions
 def plot_gauge(cpu_value, title):
     cpu_percentage = cpu_value * 100
     if cpu_percentage < 80:
@@ -57,101 +55,140 @@ def plot_scatter(df, xlim, ylim):
     source = df.copy()
     
     chart = alt.Chart(source).mark_circle().encode(
-
-        x = alt.X('downlink', scale=alt.Scale(domain=[0, xlim])),
-        y = alt.Y('uplink', scale=alt.Scale(domain=[0, ylim])),
-        color=alt.Color('category', scale=alt.Scale(domain=['safe','moderate risk','dangerous'], range=['green','yellow','red'])) 
+        x=alt.X('downlink', scale=alt.Scale(domain=[0, xlim])),
+        y=alt.Y('uplink', scale=alt.Scale(domain=[0, ylim])),
+        color=alt.Color('category', scale=alt.Scale(domain=['safe','moderate risk','dangerous'], range=['green','yellow','red']))
     ).interactive()
 
     st.altair_chart(chart, use_container_width=True)
 
-def color(risk):
-    if risk > 0.6:
-        return "red"
-    elif risk < 0.3:
-        return "green"
-    else:
-        return "yellow"
-
+# Styling Functions
 def link_risk_icon(risk_analysis_result):
     if risk_analysis_result == "Mean Risk drop or steady risk":
         return "https://i.imgur.com/17lYTAp.png"
     elif risk_analysis_result == "Significant Mean Risk increase":
         return "https://i.imgur.com/sWC2ygJ.png"
-    
+
+def font_style():
+    return(
+    st.markdown("""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+        .title {
+            font-family: 'Inter', sans-serif;
+            font-weight: 700;
+            font-size: 36px;
+        }
+        .section_title {
+            font-family: 'Inter', sans-serif;
+            font-weight: 300;
+            font-size: 20px;
+                }
+        .status_text {
+            font-family: 'Inter', sans-serif;
+            font-weight: 500;
+            font-size: 20px;
+                }
+        .progress_text {
+            font-family: 'Inter', sans-serif;
+            font-weight: 300;
+            font-size: 14px;
+                }
+        </style>
+        """, unsafe_allow_html=True))
+
+def add_vertical_space(lines: int = 1):
+    for _ in range(lines):
+        st.write("")
+
 def main():
-    st.set_page_config(page_title="Failure Predictor", page_icon=":material/search_insights:", layout="centered", initial_sidebar_state="auto")
-    
-    st.title(":material/search_insights: FAILURE PREDICTOR")
-    
+
+    #General Configuration and Styling
+    st.set_page_config(page_title="Failure Predictor", page_icon=":material/search_insights:", layout="wide", initial_sidebar_state="auto")
     app = Utils()
-    
+    font_style()
+
+    st.title(":material/search_insights: FAILURE PREDICTOR", anchor=False)
+    add_vertical_space(1)
+
+    #About Section
+    c1, c2 = st.columns([0.6, 0.4])
+    with c1:
+        with st.expander("About"):
+            st.markdown("""     
+                This app is a real-time failure predictor, collecting data from a network server and predicting the likelihood of a failure.
+                Also provides a risk assessment based on the predicted likelihood of a failure and displays the status of the prediction, the
+                CPU usage, the risk level, and the risk assessment.
+            """)
+            st.image("https://i.imgur.com/dghLTqg.png", use_column_width=False, width=600) 
+    add_vertical_space(1)
+
+    #Progress Section
+    c3, c4 = st.columns([0.6, 0.4])
+    with c3: 
+        st.markdown("### Progress")
+        progress_cols = st.columns([0.15, 0.85])
+        with progress_cols[0]:
+            status_text = st.empty()
+        with progress_cols[1]:
+            progress_bar = st.empty() 
+        st.button("RUN", type='secondary', help="Run the Failure Predictor")
+
+    #Status and CPU Usage Section
     risk_analysis_result = "Awaiting Prediction"
+    c5, c6 = st.columns([0.4, 0.6])
+    with c5:
+        st.markdown("### Status")
+        status_cols = st.columns([0.1, 0.9], vertical_alignment="center")
+        with status_cols[0]:
+            icon = st.empty()
+        with status_cols[1]:
+            status_display = st.empty() 
+            status_display.markdown(f'<p class="status_text">{risk_analysis_result}</p>', unsafe_allow_html=True)
+    with c6:
+        st.markdown("### CPU Usage")
+        gauge_cols = st.columns(4)
+        gauge_plots = [col.empty() for col in gauge_cols] 
+
+
+    add_vertical_space(1)
     
-    with st.container():
-        c3, c4 = st.columns([2, 1])
-        with c3:
-            st.markdown("### STATUS")
-            status_cols = st.columns([0.1, 0.9])
-            with status_cols[0]:
-                icon = st.empty()
-            with status_cols[1]:
-                status_display = st.empty() 
-                status_display.write(f"{risk_analysis_result}")
-        with c4:
-            st.markdown("### PROGRESS")
-            progress_cols = st.columns([0.15, 0.85])
-            with progress_cols[0]:
-                status_text = st.empty()
-            with progress_cols[1]:
-                progress_bar = st.empty() 
-            st.button("RUN", type='secondary', help="Run the Failure Predictor")
-        
-        c1, c2 = st.columns([2, 1]) 
-        with c1:
-            st.markdown("### RISK LEVEL")
-            last_rows = []
-            chart = st.line_chart(last_rows)
-
-        with c2:
-            st.markdown("### CPU USAGE")
-            gauge_cols = st.columns(2)
-            gauge_plots = [col.empty() for col in gauge_cols] 
-
-            gauge_cols2 = st.columns(2) 
-            gauge_plots.extend([col.empty() for col in gauge_cols2]) 
-
-        st.markdown("### RISK ASSESSMENT\nDownlink x Uplink")
-        chart_data3 = pd.DataFrame()
+    #Risk Assessment Section and Risk Level
+    c7, c8 = st.columns([0.5, 0.5], gap="large") 
+    with c7:
+        st.markdown("### RISK ASSESSMENT\nDOWNLINK X UPLINK")
         chart_3 = st.empty()
-        st.image("https://i.imgur.com/dghLTqg.png")
 
-        
-        for i in range(1, 100, 2): 
-                sample = app.get_sample()
-                pred = [app.make_pred(sample)]
-                risk_analysis_result = app.risk_analysis(pred)
-                status_display.write(f"{risk_analysis_result}")  
-                icon.image(link_risk_icon(risk_analysis_result))
+    with c8:
+        st.markdown("### RISK LEVEL")
+        last_rows = []
+        chart = st.line_chart(last_rows)
 
-                with c1:
-                    chart.add_rows(pred)
+    #Main Loop
+    for i in range(1, 100, 2): 
+        sample = app.get_sample()
+        pred = [app.make_pred(sample)]
+        risk_analysis_result = app.risk_analysis(pred)
+        status_display.markdown(f'<p class="status_text">{risk_analysis_result}</p>', unsafe_allow_html=True)
+        icon.image(link_risk_icon(risk_analysis_result))
 
-                cpu_values = sample[['cpu_1', 'cpu_2', 'cpu_3', 'cpu_4']].values[0]
-                for idx, cpu_val in enumerate(cpu_values):
-                    gauge_plots[idx].plotly_chart(plot_gauge(cpu_val, f'CPU {idx+1}'), use_container_width=True)
+        with c1:
+            chart.add_rows(pred)
 
-                with chart_3:
-                    app.update_data(sample, pred[0])
-                    plot_scatter(app.get_data(), 100000, 60000)
-                status_text.text("%i%%" % i)
-                progress_bar.progress(i)  
+        cpu_values = sample[['cpu_1', 'cpu_2', 'cpu_3', 'cpu_4']].values[0]
+        for idx, cpu_val in enumerate(cpu_values):
+            gauge_plots[idx].plotly_chart(plot_gauge(cpu_val, f'CPU {idx+1}'), use_container_width=True)
 
-                time.sleep(1)
- 
+        with chart_3:
+            app.update_data(sample, pred[0])
+            plot_scatter(app.get_data(), 100000, 60000)
 
+        status_text.markdown(f'<p class="progress_text">{i}% complete</p>', unsafe_allow_html=True)
+        progress_bar.progress(i)  
+        #progress_bar.progress(i, f'{i}% complete')  
 
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
-
